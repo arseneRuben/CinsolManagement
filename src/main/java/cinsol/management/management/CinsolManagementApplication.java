3ed6305606f228d3d940dc96c5cd9ef10e2349f0
Arsene Fokam P <2210100@isi-montreal.com>
2023-10-17 15:52:51 -0400

    starter

---
+++ src/main/java/cinsol/management/management/CinsolManagementApplication.java
@@ -0,0 +1,13 @@
+package cinsol.management.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinsolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinsolManagementApplication.class, args);
+	}
+
+}
